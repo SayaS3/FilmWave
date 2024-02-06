d56c3406865b3fdd0078aba3af2c220d2bf9fa3a
sayas <mrvount@gmail.com>
2024-02-06 15:15:38 +0100

    changed layout

--- src/main/java/com/sayas/filmhub/FilmWaveApplication.java
+++ src/main/java/com/sayas/filmhub/FilmWaveApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class FilmHubApplication {
+public class FilmWaveApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(FilmHubApplication.class, args);
+		SpringApplication.run(FilmWaveApplication.class, args);
 	}
 
 }
--- src/test/java/com/sayas/filmhub/FilmWaveApplicationTests.java
+++ src/test/java/com/sayas/filmhub/FilmWaveApplicationTests.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class FilmHubApplicationTests {
+class FilmWaveApplicationTests {
 
 	@Test
 	void contextLoads() {
